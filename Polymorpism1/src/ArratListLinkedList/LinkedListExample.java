a0c6b881061fa951f19823b1bf0b0288be3941e7
PC14-07 <PC14-07@wsh>
2022-06-23 16:32:00 +0900

    수정

--- Polymorpism1/src/ArratListLinkedList/LinkedListExample.java
+++ Polymorpism1/src/ArratListLinkedList/LinkedListExample.java
@@ -6,8 +6,8 @@ import java.util.*;
 public class LinkedListExample {
 
 	public static void main(String[] args) {
-	    List<String> list1 = new ArrayList<>();
-		List<String> list2 = new LinkedList<>();
+	    List<String> list2 = new ArrayList<>();
+		List<String> list3 = new LinkedList<>();
 
 
 		long startTime;
