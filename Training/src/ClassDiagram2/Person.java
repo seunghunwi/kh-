c585ea03b65cd1f2c314fbe0a726abaf734e8b4c
seunghunwi <whdmltnr159@naver.com>
2022-06-29 15:02:29 +0900

    다이아그램을 보고 실습

---
+++ Training/src/ClassDiagram2/Person.java
@@ -0,0 +1,12 @@
+package ClassDiagram2;
+
+public class Person {
+        //클래스 다이어 그램을 보고 작성
+	    //사용관계
+  		Phone homePhone = new Phone();
+		Phone officePhone = new Phone();
+		
+
+	
+
+}
---
+++ Training/src/ClassDiagram2/Phone.java
@@ -0,0 +1,29 @@
+package ClassDiagram2;
+
+public class Phone {
+	private String homePhone;
+	private String officePhone;
+	
+
+	public String getHomePhone() {
+		return homePhone;
+	}
+
+
+	public void setHomePhone(String homePhone) {
+		this.homePhone = homePhone;
+	}
+
+
+	public String getOfficePhone() {
+		return officePhone;
+	}
+
+
+	public void setOfficePhone(String officePhone) {
+		this.officePhone = officePhone;
+	}
+
+
+	
+}
