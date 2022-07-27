b26a3f96d1b55945d5d311d3aa3da39f6a6beb8c
seunghunwi <whdmltnr159@naver.com>
2022-07-27 18:55:05 +0900

    중첩 클래스 실습 2차

--- Polymorpism1/src/NestedClass1/A.java
+++ Polymorpism1/src/NestedClass1/A.java
@@ -2,48 +2,45 @@ package NestedClass1;
 
 public class A {
 	A() {System.out.println("A 객체를 생성함"); }
-	
-	public class B {
+
+	 class B {
 		B(){System.out.println("B 객체를 생성함");}
 		int field1;
 		//static int field2;
 		void method() {}
-		//staticd void method2() {}
-		
-		
+		//static void method2() {}	
 	}
-		
-		static class C {
-			C() { System.out.println("C 객체를 생성됨");}
+
+	static class C {
+		C() { System.out.println("C 객체를 생성됨");}
+		int field1;
+		static int field2;
+		void method() {}
+		static void method2() {}
+	}
+
+
+
+	void method() {
+		//로컬 클래스
+		class D {
+			D(){System.out.println("D 객체를 생성함");}
 			int field1;
-			static int field2;
-			void method() {}
-			static void method2() {}
-			}
-			
-			
-		
-			void method() {
-				//로컬 클래스
-				
-				class D {
-					D() {System.out.println("D 객체를 생성함");}
-					int field1;
-					//static int field2;
-					void method1() {}
-					//static void method(){}
-					
-					}
-					D d= new D();
-					d.field1 = 3;
-					d.method1();
-				
-					
-				}
-			}
-		
-	
-	
-	
+			//static int field2;
+			void method1() {}
+			//static void method2(){}
+
+		}
+		D d= new D();
+		d.field1 = 3;
+		d.method1();
+
+
+	}
+}
+
+
+
+
 
 
--- Polymorpism1/src/NestedClass1/Main.java
+++ Polymorpism1/src/NestedClass1/Main.java
@@ -14,7 +14,14 @@ public class Main {
 		A.C c = new A.C();
 		c.field1 = 3;
 		c.method();
+		
+		// 로컬 클래스 객체 생성을 위한 메소드 호출
+		a.method();
 
 	}
 
 }
+
+
+
+
